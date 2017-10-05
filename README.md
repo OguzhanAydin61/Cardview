23464fc34fc773ac7f43d9205ec2da8525462591
OguzhanAydin61 <oguzhanaydin61@hotmail.com>
2017-10-05 16:40:30 +0300

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cardview
+Test project for cardview
+# Projeye Veri Ekleme
+prepareAlbums() metodu içinde ekleme yapılacak kısım yazılıyor.
+
+- İstek Yönetemleri: Belirlenen retrofit yapısına göre oluyor. 
+Post veya Get ile oluyor.
+#Proje UI
+- Burda cardview başlıklı RecyclerView Adapter kullandık. Adapter içindeki her menude :
+
+    - Add to Favourites
+    - Go to Place 
+    
+    
+    *  Özellikleri mevcuttur.
+  
---
+++ app/src/main/java/info/androidhive/cardview/Album.java
@@ -0,0 +1,43 @@
+package info.androidhive.cardview;
+
+/**
+ * Created by Lincoln on 18/05/16.
+ */
+public class Album {
+    private String name;
+    private int numOfSongs;
+    private String thumbnail;
+
+    public Album() {
+    }
+
+    public Album(String name, int numOfSongs, String thumbnail) {
+        this.name = name;
+        this.numOfSongs = numOfSongs;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfSongs() {
+        return numOfSongs;
+    }
+
+    public void setNumOfSongs(int numOfSongs) {
+        this.numOfSongs = numOfSongs;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
